b97754ac7164d9190fe54179bbc2deddd1451417
ahspence53 <147179766+ahspence53@users.noreply.github.com>
2026-04-21 16:46:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -72,6 +72,8 @@ const txEndDate = document.getElementById("tx-end-date");
 
   const txAccount = document.getElementById("tx-account");
 
+  account: txAccount.value || "Santander",
+
   const CACHE_VERSION = "v1.5.5";
 const CACHE_NAME = `budget-app-${CACHE_VERSION}`;
 const APP_VERSION = `budget-app-${CACHE_VERSION}`;
